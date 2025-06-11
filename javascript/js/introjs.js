ac39d46e056cb30538709a4bc079cf7caf1c1c43
Mario Cortes <>
2025-06-11 10:37:47 -0600

    Section5: Javascript

---
+++ javascript/js/introjs.js
@@ -0,0 +1,16 @@
+/* COMENTARIO 
+MULTILINEA*/
+
+// Alerta en JS
+     alert("Hola mundo!");
+     alert("Hola mundo2!");
+
+// CONSOLA EN JS
+     console.log('Hola, saludamos a la consola!');
+     console.log(2+2);
+     console.info('Mostrando una informacion');
+     console.warn('Estamos mostrando una advertencia');
+     console.error('Mostrando un error');*/
+
+//Document Write
+     document.write('Hola este mensaje se mostrará en mi web');
